2270509839e0a5c35700b0870d55b57a18d446f2
Taeph <rkdxowns333@gmail.com>
2020-08-24 17:07:13 +0900

    '카트에서 주문내역 결제 완료시 DB (주문정보, 주문음식정보) 에 INSERT'

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ var expressLayouts = require("express-ejs-layouts");
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "root",
-  database: "mydb",
+  password: "12qw!@QW",
+  database: "test",
 });
 
 // views
@@ -39,22 +39,28 @@ app.get("/home", function (req, res) {
 app.get("/restAreaInfo", function (req, res) {
   res.render("home/restAreaInfo");
 });
+
 //메뉴 목록 화면
 app.get("/menulist", function (request, response) {
   response.render("home/menulist");
 });
+
 //메뉴고르고 결제할때 화면
 app.get("/payment", function (request, response) {
   response.render("home/payment");
 });
+
 //장바구니 화면
 app.get("/cart", function (request, response) {
   response.render("cart/cart");
 });
+
 //주문했었던 목록 화면(주문번호, 주문내역 확인)
 app.get("/orderlist", function (request, response) {
   response.render("orderlist/orderlist");
 });
+
+// 휴게소 정보 넘겨주기
 app.get("/restAreaInfo", function (req, res) {
   res.render("restAreaInfo");
 });
@@ -73,6 +79,9 @@ app.get("/orderList", function (req, res) {
 app.listen(3000, function () {
   console.log("example app listening at http://localhost:3000");
 });
+
+
+
 
 // 사용자 앱에서 휴게서 위도,경도 요청시 값 보내주기 - 홈 화면(지도, 리스트)
 app.post("/requestRestAreaLatLong", function (req, res) {
@@ -133,3 +142,99 @@ getMenuInfo = async (areaName) => {
   // console.log(res.data.list);
   return res.data.list;
 };
+
+
+// 결제완료시 음식 주문내역 DB에 삽입
+app.post('/insertOrderList', async function(req, res) {
+    console.log(req.body);
+    const phoneNo = req.body.phone_no;
+    const totalCost = req.body.total_cost;
+    const areaNm = req.body.area_nm;
+    const lists = req.body.lists;
+    const jsonData = JSON.parse(lists);
+
+    console.log('orderNo start');
+    const order_no = await getOrderNo();
+    console.log('orderNo end');
+
+    console.log(phoneNo);
+
+    const SQL1 = {
+        order_no : order_no,
+        area_nm : areaNm,
+        total_cost : totalCost,
+        orderer_pn : phoneNo,
+    }
+    // 주문정보 INSERT
+    connection.query('INSERT INTO order_info_tb SET ? ', SQL1, function(error, result, fields) {
+        if(error) {
+            throw error;
+        } else {
+            console.log(result);
+
+            // 음식 리스트 INSERT
+            for(i in jsonData) {
+                console.log(jsonData[i]);
+                const name = jsonData[i].item_name;
+                const cnt = jsonData[i].qty;
+                const price = jsonData[i].price;
+        
+                const SQL2 = {
+                    order_no: order_no,
+                    food_nm: name,
+                    food_cnt: cnt,
+                    food_price: price,
+                };
+        
+                connection.query('INSERT INTO order_food_info_tb SET ?', SQL2, function(error, result, fields) {
+                    if(error) {
+                        throw error;
+                    } else {
+                        console.log(result);
+                    }
+                });
+            }
+        }
+        // 주문정보, 음식주문정보 테이블에 다 넣으면 주문번호 반환
+        res.send(order_no);
+    });
+})
+
+// 주문번호 만들기
+async function getOrderNo() {
+    // 오늘 날짜 yyyymmdd 형태로 만들기
+    const date = new Date();
+    const year = date.getFullYear().toString();
+    const month = (date.getMonth() + 1);
+    const day = date.getDate();
+    const todayDate = year + (month < 10 ? ('0' + month) : month).toString() + (day < 10 ? ('0' + day) : day).toString();
+    console.log('today:', todayDate);
+    
+    // 오늘 날짜 기준으로 주문 건수 계산해서 다음 번호 붙이기
+    const cnt = await getOrderCnt(todayDate);
+    console.log('count :', cnt);
+
+    // 주문번호 만들기
+    const orderNo = 'OD' + todayDate + cnt;
+    console.log('in order No:', orderNo);
+
+    return orderNo;
+}
+
+// DB에서 오늘 날짜 기준으로 들어온 주문 건수 가져오기 (동기 처리 해야함)
+function getOrderCnt(todayDate) {
+    return new Promise( (resolve, reject) => {
+        let cnt = 0;
+        connection.query('SELECT * FROM order_info_tb WHERE substr(order_no, 3, 8) = ?', [todayDate], function(error, result, fields) {
+            if(error) {
+                reject(error);
+            } else {
+                console.log('result length:', result.length);
+                let temp = result.length + 1;
+                cnt = (temp < 10 ? '0000' + temp : (temp < 100 ? '000' + temp : (temp < 1000 ? '00' + temp : (temp < 10000 ? '0' + temp : temp))));
+                console.log('getOrderCnt:',cnt);
+                resolve(cnt);
+            }
+        });
+    })
+}
